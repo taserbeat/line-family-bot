683d0f988a8d01734e19ac3d9ba5002e3fc37d64
taserbeat <taserbeat@gmail.com>
2021-08-01 17:28:36 +0900

    今日は何の日をスクレイピングするAPIを追加

---
+++ modules/functions/scraping_test.go
@@ -0,0 +1,14 @@
+package functions_test
+
+import (
+	"testing"
+
+	"github.io/taserbeat/line-family-bot/modules/functions"
+)
+
+func TestGetToday(t *testing.T) {
+	_, err := functions.GetToday()
+	if err != nil {
+		t.Errorf("「今日は何の日」の取得エラー: %v", err)
+	}
+}
